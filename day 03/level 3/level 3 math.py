eb215e03d47b3af7d0b24df90b1ec50ad53afa3b
a13412 <asa319707@gmail.com>
2025-03-28 21:01:59 +0400

    first commit

---
+++ day 03/level 3/level 3 math.py
@@ -0,0 +1,79 @@
+print(55 + 14)
+
+print(219 + 201)
+
+print(893 + 1103)
+
+print(513 + 84)
+
+print(743 + 1346)
+
+print(1624 + 1626)
+
+print(875+912)
+
+print(1109 +  1245)
+
+print(1324+68)
+
+print(239 + 201)
+
+print(" ")
+
+
+
+print(124 - 43)
+
+print(7534 - 1275)
+
+print(615 - 505)
+
+print(776 - 413)
+
+print(747 - 590)
+
+print(802 - 546)
+
+print(2040 - 2016)
+
+print(3456 - 2341)
+
+print(9891 - 6795)
+
+print(651 - 147)
+
+
+print(" ")
+
+
+
+
+print(68 / 2)
+print(33 / 3)
+print(56 / 4)
+print(345 / 5)
+print(250 / 25)
+print(63 / 3)
+print(66 / 6)
+print(981 / 9)
+print(864 / 2)
+print(789 / 3)
+
+
+print(" ")
+
+
+print(43 * 2)
+print(8 * 11)
+print(91 * 32)
+print(32 * 12)
+print(21 * 22)
+print(56 * 9)
+print(65 * 19)
+print(13 * 16)
+print(72 * 42)
+print(15 * 25)
+print(46 * 4)
+
+
+
